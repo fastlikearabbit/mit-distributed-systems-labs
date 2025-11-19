a2e10af6fe8ff9bd34ae7913dfa7eaa2f40a5751
Daniel Ghenghea <daniel.ghenghea2000@gmail.com>
2025-11-19 17:46:33 +0200

    start 3D

--- src/raft1/raft.go
+++ src/raft1/raft.go
@@ -168,6 +168,28 @@ func (rf *Raft) PersistBytes() int {
 	return rf.persister.RaftStateSize()
 }
 
+type InstallSnapshotArgs struct {
+	Term              int
+	LeaderId          int
+	LastIncludedIndex int
+	LastIncludedTerm  int
+	Data              []byte
+}
+
+type InstallSnapshotReply struct {
+	Term int
+}
+
+func (rf *Raft) InstallSnapshot(args *InstallSnapshotArgs, reply *InstallSnapshotReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+}
+
+func (rf *Raft) sendInstallSnapshot(server int, args *InstallSnapshotArgs, reply *InstallSnapshotReply) bool {
+	ok := rf.peers[server].Call("Raft.InstallSnapshot", args, reply)
+	return ok
+}
+
 // the service says it has created a snapshot that has
 // all info up to and including index. this means the
 // service no longer needs the log through (and including)
@@ -480,7 +502,6 @@ func (rf *Raft) sendHeartBeats() {
 			if server == rf.me {
 				continue
 			}
-
 			go rf.sendAppendEntriesToFollower(server)
 		}
 		rf.mu.Unlock()
