82d47a0bff52d950609627506288e7b2e8436c60
Daniel Ghenghea <daniel.ghenghea2000@gmail.com>
2025-11-20 14:35:49 +0200

    start lab4

--- src/kvraft1/rsm/rsm.go
+++ src/kvraft1/rsm/rsm.go
@@ -2,24 +2,28 @@ package rsm
 
 import (
 	"sync"
+	"sync/atomic"
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
 	"6.5840/raft1"
 	"6.5840/raftapi"
 	"6.5840/tester1"
-
 )
 
 var useRaftStateMachine bool // to plug in another raft besided raft1
 
-
 type Op struct {
-	// Your definitions here.
-	// Field names must start with capital letters,
-	// otherwise RPC will break.
+	Me  int
+	Id  int
+	Req any
 }
 
+var idCounter uint64 = 0
+
+func GenerateId() uint64 {
+	return atomic.AddUint64(&idCounter, 1)
+}
 
 // A server (i.e., ../server.go) that wants to replicate itself calls
 // MakeRSM and must implement the StateMachine interface.  This
@@ -74,7 +78,6 @@ func MakeRSM(servers []*labrpc.ClientEnd, me int, persister *tester.Persister, m
 func (rsm *RSM) Raft() raftapi.Raft {
 	return rsm.rf
 }
-
 
 // Submit a command to Raft, and wait for it to be committed.  It
 // should return ErrWrongLeader if client should find new leader and
