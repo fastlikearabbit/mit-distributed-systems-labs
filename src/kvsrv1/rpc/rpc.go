c90dee448c7ec8b1536d0c57ec3b1a77d20734ce
Daniel Ghenghea <daniel.ghenghea2000@gmail.com>
2026-05-11 20:40:55 +0300

    feat(rpc): add txn support

--- src/kvsrv1/rpc/rpc.go
+++ src/kvsrv1/rpc/rpc.go
@@ -7,6 +7,7 @@ const (
 	OK         = "OK"
 	ErrNoKey   = "ErrNoKey"
 	ErrVersion = "ErrVersion"
+	ErrTxn     = "ErrTxn"
 
 	// Err returned by Clerk only
 	ErrMaybe = "ErrMaybe"
@@ -38,3 +39,74 @@ type GetReply struct {
 	Err     Err
 }
 
+type TxnCompareTarget string
+
+const (
+	TxnCompareVersion TxnCompareTarget = "version"
+	TxnCompareValue   TxnCompareTarget = "value"
+)
+
+type TxnCompareResult string
+
+const (
+	TxnCompareEqual    TxnCompareResult = "="
+	TxnCompareNotEqual TxnCompareResult = "!="
+	TxnCompareGreater  TxnCompareResult = ">"
+	TxnCompareLess     TxnCompareResult = "<"
+)
+
+type TxnCompare struct {
+	Key     string
+	Target  TxnCompareTarget
+	Result  TxnCompareResult
+	Value   string
+	Version Tversion
+}
+
+func CmpVersion(key string, result TxnCompareResult, version Tversion) TxnCompare {
+	return TxnCompare{Key: key, Target: TxnCompareVersion, Result: result, Version: version}
+}
+
+func CmpValue(key string, result TxnCompareResult, value string) TxnCompare {
+	return TxnCompare{Key: key, Target: TxnCompareValue, Result: result, Value: value}
+}
+
+type TxnOpKind string
+
+const (
+	TxnOpGet TxnOpKind = "get"
+	TxnOpPut TxnOpKind = "put"
+)
+
+type TxnOp struct {
+	Kind  TxnOpKind
+	Key   string
+	Value string
+}
+
+func OpGet(key string) TxnOp {
+	return TxnOp{Kind: TxnOpGet, Key: key}
+}
+
+func OpPut(key, value string) TxnOp {
+	return TxnOp{Kind: TxnOpPut, Key: key, Value: value}
+}
+
+type TxnArgs struct {
+	Compare []TxnCompare
+	Then    []TxnOp
+	Else    []TxnOp
+}
+
+type TxnOpResponse struct {
+	Kind    TxnOpKind
+	Value   string
+	Version Tversion
+	Err     Err
+}
+
+type TxnReply struct {
+	Succeeded bool
+	Responses []TxnOpResponse
+	Err       Err
+}
